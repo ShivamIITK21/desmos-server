159ad07978f1362e7ae05e20dd9abc19e0c97f86
Shivam <sshivam21@iitk.ac.in>
2024-02-03 02:13:57 +0530

    Added code for websockets

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import(
+    "net/http"
     "github.com/ShivamIITK21/desmos-server/http-listener"
+    "github.com/ShivamIITK21/desmos-server/websocket-server"
 )
 
 func main(){
     httpListener := httplistener.NewHttpListener()
-    httpListener.Run(":8080")
+    websocketserver.NewWebSockConnection()
+    go http.ListenAndServe(":8081", nil)
+    go httpListener.Run(":8080")
+    for {}
 }
---
+++ websocket-server/ws-server.go
@@ -0,0 +1,54 @@
+package websocketserver
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+}
+
+type WSConnection struct{
+    Conn    *websocket.Conn
+    Mut     sync.RWMutex 
+}
+
+func NewWebSockConnection() *WSConnection{
+    wsconn := WSConnection{}
+    http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request){
+        upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+        conn, err := upgrader.Upgrade(w, r, nil) 
+        if err != nil {
+            log.Fatalln(err)
+        }
+        wsconn.Conn = conn
+        
+        for{
+            msgType, msg, err := conn.ReadMessage()
+            if err != nil {
+                return
+            }
+
+            // Print the message to the console
+            fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+            // Write message back to browser
+            if err = conn.WriteMessage(msgType, msg); err != nil {
+                return
+            }
+        }
+
+    })
+    return &wsconn
+}
+
+func (*WSConnection) Run(port string){
+    fmt.Printf("Starting the websocket connection at %s", port)
+    http.ListenAndServe(port, nil)
+}
